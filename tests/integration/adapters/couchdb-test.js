c6a6aa0c313ebf09bfafb5bb474053f2e2360f46
Brennan Falkner <Brennan@bfalkner.com>
2014-11-12 10:36:17 -0500

    Test record creation

--- tests/integration/adapters/couchdb-test.js
+++ tests/integration/adapters/couchdb-test.js
@@ -7,9 +7,10 @@ module('CouchDB Adapter', {
   setup: function() {
     App = startApp();
     this.store = App.__container__.lookup('store:main');
+    this.db = this.store.adapterFor({ typeKey: 'post' }).get('db');
   },
   teardown: function() {
-    this.store.adapterFor({ typeKey: 'post' }).get('db').destroy();
+    this.db.destroy();
     Ember.run(App, 'destroy');
   }
 });
@@ -17,16 +18,22 @@ module('CouchDB Adapter', {
 test('Model#save', function() {
   var _this = this,
       record = Ember.run(function() {
-        return _this.store.createRecord('post', {id: 1});
+        return _this.store.createRecord('post', { id: 1, name: "Rails is omakase" });
       });
 
   stop();
   Ember.run(function() {
     record.save().then(function() {
-      equal(record.get("isNew"), false, "Record should be saved.");
-    }, function() {
-      ok(false, "Should be successful.");
+      equal(record.get("isNew"), false, "Record should be saved");
+      return _this.db.get('post::1').then(function(doc) {
+        ok(doc, "Document should exist in database");
+        equal(record.get('data.rev'), doc._rev, "Revision should be set on Record data");
+        equal(doc.name, "Rails is omakase", "Data should be set");
+      }, function(err) {
+        ok(false, "Document should exist in database, message: " + err.message);
+      });
+    }).catch(function() {
+      ok(false, "Should be successful");
     }).finally(function() { start(); });
   });
-
 });
